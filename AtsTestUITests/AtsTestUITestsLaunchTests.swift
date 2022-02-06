52ccb0e15dab22d9977fd925c40097bbad361dc6
Ilja Rozhko <ilja@oktobyte.com>
2022-02-06 12:37:56 +0100

    Initial Commit

---
+++ AtsTestUITests/AtsTestUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AtsTestUITestsLaunchTests.swift
+//  AtsTestUITests
+//
+//  Created by Ilja Rozhko on 06.02.22.
+//
+
+import XCTest
+
+class AtsTestUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
